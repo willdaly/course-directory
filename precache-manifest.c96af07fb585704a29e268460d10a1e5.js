31fda0d11a652ab50ebe575ee0ad5f51290f875e
Will Daly <rwilliamdaly@gmail.com>
2019-09-25 15:51:33 -0400

    Updates

---
+++ precache-manifest.c96af07fb585704a29e268460d10a1e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "2b640f486c5889ba1a01",
+    "url": "/course-directory/static/js/main.2b640f48.chunk.js"
+  },
+  {
+    "revision": "f5ae2494a8dbd9f95f4b",
+    "url": "/course-directory/static/js/1.f5ae2494.chunk.js"
+  },
+  {
+    "revision": "2b640f486c5889ba1a01",
+    "url": "/course-directory/static/css/main.da351561.chunk.css"
+  },
+  {
+    "revision": "230327a3b3cdf7a0b9185ee2fca41963",
+    "url": "/course-directory/index.html"
+  }
+];
